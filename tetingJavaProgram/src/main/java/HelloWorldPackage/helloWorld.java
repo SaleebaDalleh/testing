2595d0f0f47b69f091a79fa8dedad71e0b3c52fb
SaleebaDalleh <saleeba.dalleh@gmail.com>
2019-10-21 09:55:59 +0300

    merge goeroge branch

--- tetingJavaProgram/src/main/java/HelloWorldPackage/helloWorld.java
+++ tetingJavaProgram/src/main/java/HelloWorldPackage/helloWorld.java
@@ -2,11 +2,12 @@ package HelloWorldPackage;
 
 public class helloWorld {
 
-	
-	
 	public static void main(String[] args) {
 		System.out.println("hello");
-		
+
 		System.out.println("the branch name is saleebaBr whicj im working on ");
+
+		System.out.println("George's code");
+
 	}
 }
