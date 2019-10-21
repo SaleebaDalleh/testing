b818b5398b55746e62e12ed6351a9f1af10d15b8
SaleebaDalleh <saleeba.dalleh@gmail.com>
2019-10-21 09:35:20 +0300

    change on branch saleebaBr

--- tetingJavaProgram/src/main/java/HelloWorldPackage/helloWorld.java
+++ tetingJavaProgram/src/main/java/HelloWorldPackage/helloWorld.java
@@ -6,5 +6,7 @@ public class helloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("hello");
+		
+		System.out.println("the branch name is saleebaBr whicj im working on ");
 	}
 }
